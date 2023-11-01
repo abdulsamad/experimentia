29ea5ca5f38a274e0ea0a715e985678c58c14fb9
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-11-02 02:32:39 +0530

    refactor: Added more languages

--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -1,8 +1,18 @@
 export const languages = [
 	{
 		code: 'en-IN',
-		text: 'English',
+		text: 'English (Indian)',
 		selected: true,
+	},
+	{
+		code: 'en-US',
+		text: 'English (United States)',
+		selected: false,
+	},
+	{
+		code: 'en-UK',
+		text: 'English (United Kingdom)',
+		selected: false,
 	},
 	{
 		code: 'hi-IN',
@@ -10,8 +20,18 @@ export const languages = [
 		selected: false,
 	},
 	{
+		code: 'ur-IN',
+		text: 'Urdu',
+		selected: false,
+	},
+	{
 		code: 'ar-EG',
 		text: 'Arabic',
+		selected: false,
+	},
+	{
+		code: 'tr-TR',
+		text: 'Turkish',
 		selected: false,
 	},
 ];
