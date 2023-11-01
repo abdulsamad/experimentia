0b7ad101513d960b6e3f35251c6c9790b2136f4d
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-11-02 03:08:03 +0530

    refactor: Improved logic for default language

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -33,9 +33,10 @@ const Editor = () => {
 			</div>
 			<select
 				className='bg-gray-50 my-12 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+				defaultValue={languages.find(({ selected }) => selected)?.code}
 				onChange={changeLanguage}>
-				{languages.map(({ code, text, selected }) => (
-					<option key={code} value={code} selected={selected}>
+				{languages.map(({ code, text }) => (
+					<option key={code} value={code}>
 						{text}
 					</option>
 				))}
