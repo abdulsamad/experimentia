8ad8ab2032e13645d67fb3906af5c5d88c7d50f7
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-11-05 18:55:11 +0530

    refactor: Added redirect for unauth users

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,17 @@
 'use client';
+import React from 'react';
+import { redirect } from 'next/navigation';
 import { Provider } from 'jotai';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 import store from '@/store';
 import Editor from '@/components/Editor';
 
 const Home = () => {
+	const { user } = useUser();
+
+	if (!user) redirect('/');
+
 	return (
 		<Provider store={store}>
 			<main className='p-5'>
