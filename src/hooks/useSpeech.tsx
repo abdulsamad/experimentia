84ada55ade11c93e8f7a0b44430e0a3d36368aea
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-11-05 12:23:17 +0530

    refactor: Abstracted speech

--- src/hooks/useSpeech.tsx
+++ src/hooks/useSpeech.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef, useCallback } from 'react';
-import { useSetAtom, useAtom } from 'jotai';
+import { useSetAtom } from 'jotai';
 
 import { editorAtom } from '@/store';
 import { speechLog, getCorrectedText, speechGrammer } from '@/utils';
@@ -8,11 +8,10 @@ const speechRecognition = new (webkitSpeechRecognition || SpeechRecognition)();
 const speechRecognitionList = new webkitSpeechGrammarList();
 
 const useSpeech = ({ editor }: { editor: any }) => {
+	const setEditorState = useSetAtom(editorAtom);
 	const [isListening, setIsListening] = useState(false);
 
 	const recognition = useRef<SpeechRecognition | null>(null);
-
-	const [state, setState] = useAtom(editorAtom);
 
 	useEffect(() => {
 		speechRecognitionList.addFromString(speechGrammer, 1);
@@ -23,31 +22,10 @@ const useSpeech = ({ editor }: { editor: any }) => {
 		recognition.current.lang = 'en-IN';
 		recognition.current.interimResults = false;
 		recognition.current.maxAlternatives = 1;
-
-		recognition.current.onresult = (event: any) => {
-			const results = event.results;
-			const len = Object.keys(results).length;
-
-			// TODO: Clean log results
-			console.log(results);
-
-			const transcript = results[len - 1][0].transcript;
-			setState(transcript);
-
-			getCorrectedText(transcript, recognition.current?.lang).then(
-				({ chatCompletion }) => {
-					const { choices } = chatCompletion;
-					setState(
-						`\n<b>Corrected:</b> <em>${choices[0]?.message?.content}</em>`,
-					);
-					console.log({ chatCompletion });
-				},
-			);
-		};
-
+		recognition.current.onresult = onSpeechResult;
 		recognition.current.onspeechend = stopRecognition;
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [setState]);
+	}, []);
 
 	const startRecognition = useCallback(async () => {
 		if (!recognition.current) return null;
@@ -70,6 +48,30 @@ const useSpeech = ({ editor }: { editor: any }) => {
 		speechLog('Stopped');
 		setIsListening(false);
 	}, []);
+
+	const onSpeechResult = useCallback(
+		async (ev: SpeechRecognitionEvent) => {
+			const results = ev.results;
+			const len = Object.keys(results).length;
+
+			// TODO: Clean log results
+			console.log(results);
+
+			const transcript = results[len - 1][0].transcript;
+			setEditorState(transcript);
+
+			getCorrectedText(transcript, recognition.current?.lang).then(
+				({ chatCompletion }) => {
+					const { choices } = chatCompletion;
+					setEditorState(
+						`\n<b>Corrected:</b> <em>${choices[0]?.message?.content}</em>`,
+					);
+					console.log({ chatCompletion });
+				},
+			);
+		},
+		[setEditorState],
+	);
 
 	return {
 		startRecognition,
