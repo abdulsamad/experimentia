ee61cf4a26cf36cec880f49e48eb7d7275c323ef
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-11-05 12:16:41 +0530

    refactor: Modify the state behaviour

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -4,8 +4,7 @@ const store = createStore();
 
 export const editorAtom = atom('', (get, set, update) => {
 	const state = get(editorAtom);
-
-	set(editorAtom, update);
+	set(editorAtom, state + update);
 });
 
 export default store;
