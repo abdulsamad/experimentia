05ef8d7972ff72d9c57f0b09c36766d5db5b7baf
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-11-12 03:45:12 +0530

    refactor: Updated hero text and UI

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -66,16 +66,16 @@ const Chats = () => {
 					);
 				})
 			) : (
-				<div className='hero h-[250px] bg-base-200 rounded-3xl shadow'>
+				<div className='hero h-[250px] bg-base-200 text-sky-200 border-2 rounded-3xl border-sky-200 shadow-[0_0_1px_#fff,inset_0_0_1px_#fff,0_0_2px_#08f,0_0_6px_#08f,0_0_15px_#08f]'>
 					<div className='hero-content text-center'>
 						<div className='max-w-md'>
 							<h1 className='text-5xl font-bold'>
-								Hello there, <span className='animate-bounce'>👋</span>
+								Hello there, <span className='animate-wave'>👋</span>
 							</h1>
 							<p className='py-6'>
 								Tap the mic button in the bottom right corner and start
-								speaking. You can also change setting from the hamburger menu in
-								the top left corner.
+								speaking. You can also change settings from the hamburger menu
+								in the top left corner.
 							</p>
 						</div>
 					</div>
