f1bb8415cfb1aaf010a03b56b94600a12626740c
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-11-14 17:38:29 +0530

    fix: Improved error handling

--- src/app/api/text/route.ts
+++ src/app/api/text/route.ts
@@ -14,49 +14,48 @@ const configCatClient = configcat.getClient(
 );
 
 export async function POST(request: Request) {
-	const { prompt, language, type, model } = await request.json();
-	const session = await getSession();
-	let isGPT4Enabled;
+	try {
+		const { prompt, language, type, model } = await request.json();
+		const session = await getSession();
+		let isGPT4Enabled;
 
-	if (!prompt)
-		return NextResponse.json({ err: 'Prompt not found' }, { status: 400 });
+		if (!prompt)
+			return NextResponse.json({ err: 'Prompt not found' }, { status: 400 });
 
-	if (!session?.user)
+		if (!session?.user)
+			return NextResponse.json(
+				{ err: 'User is not authenticated' },
+				{ status: 400 },
+			);
+
+		if (model === 'gpt-4') {
+			const user = new configcat.User(session.user.email);
+
+			// The default user will be used in the evaluation process.
+			isGPT4Enabled = await configCatClient.getValueAsync(
+				'enable-GPT-4',
+				false,
+				user,
+			);
+		}
+
+		const chatCompletion = await openai.chat.completions.create({
+			messages: [
+				{ role: 'system', content: promptMapper(type, language).prompt },
+				{ role: 'user', content: prompt },
+			],
+			model: isGPT4Enabled ? model || 'gpt-3.5-turbo' : 'gpt-3.5-turbo',
+		});
+		const { choices } = chatCompletion;
+
 		return NextResponse.json(
-			{ err: 'User is not authenticated' },
-			{ status: 400 },
+			{ success: true, chatCompletion, content: choices[0]?.message?.content },
+			{ status: 200 },
 		);
-
-	if (model === 'gpt-4') {
-		const user = new configcat.User(session.user.email);
-
-		// The default user will be used in the evaluation process.
-		isGPT4Enabled = await configCatClient.getValueAsync(
-			'enable-GPT-4',
-			false,
-			user,
+	} catch (err) {
+		return NextResponse.json(
+			{ success: false, err: 'Something went wrong!' },
+			{ status: 500 },
 		);
 	}
-
-	const chatCompletion = await openai.chat.completions.create({
-		messages: [
-			{
-				role: 'system',
-				content: promptMapper(type, language).prompt,
-			},
-			{
-				role: 'user',
-				content: prompt,
-			},
-		],
-		model: isGPT4Enabled ? model || 'gpt-3.5-turbo' : 'gpt-3.5-turbo',
-	});
-	const { choices } = chatCompletion;
-
-	return NextResponse.json(
-		{ chatCompletion, content: choices[0]?.message?.content },
-		{
-			status: 200,
-		},
-	);
 }
