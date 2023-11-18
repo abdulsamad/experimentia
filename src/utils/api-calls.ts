48dc78a3018b72832ea091b14aa452d52927280b
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-11-18 17:04:46 +0530

    feat: Added token method for authorization and cleaned up code

--- src/utils/api-calls.ts
+++ src/utils/api-calls.ts
@@ -3,16 +3,27 @@ import axios from 'axios';
 import { getConfig } from './config';
 
 const axiosInstance = axios.create({
-	baseURL: '/api',
+	baseURL: `${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/api`,
 });
 
+export const getApiAccessToken = async () => {
+	const res = await axios.get('/api/token');
+	return res.data.token.access_token;
+};
+
 export const getGeneratedText = async (prompt: string, language?: string) => {
-	const res = await axiosInstance.post('/text', {
-		prompt,
-		language,
-		type: getConfig('variation'),
-		model: getConfig('model'),
-	});
+	const token = await getApiAccessToken();
+
+	const res = await axiosInstance.post(
+		'/text',
+		{
+			prompt,
+			language,
+			type: getConfig('variation'),
+			model: getConfig('model'),
+		},
+		{ headers: { Authorization: `Bearer ${token}` } },
+	);
 
 	return res.data;
 };
@@ -22,12 +33,13 @@ export const getGeneratedImage = async (
 	size = '512x512',
 	n = 1,
 ) => {
-	const res = await axiosInstance.post('/image', {
-		prompt,
-		size,
-		n,
-		model: getConfig('model'),
-	});
+	const token = await getApiAccessToken();
+
+	const res = await axiosInstance.post(
+		'/image',
+		{ prompt, size, n, model: getConfig('model') },
+		{ headers: { Authorization: `Bearer ${token}` } },
+	);
 
 	return res.data;
 };
