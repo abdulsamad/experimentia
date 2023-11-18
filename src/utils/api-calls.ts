544ff22d4b0d2f9869b6af8666373b219fde6a6e
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-11-18 16:01:08 +0530

    refactor: Added axios for api calls

--- src/utils/api-calls.ts
+++ src/utils/api-calls.ts
@@ -1,17 +1,16 @@
+import axios from 'axios';
+
 import { getConfig } from './config';
 
 export const getGeneratedText = async (prompt: string, language?: string) => {
-	const res = await fetch('/api/text', {
-		method: 'POST',
-		body: JSON.stringify({
-			prompt,
-			language,
-			type: getConfig('variation'),
-			model: getConfig('model'),
-		}),
+	const res = await axios.post('/api/text', {
+		prompt,
+		language,
+		type: getConfig('variation'),
+		model: getConfig('model'),
 	});
 
-	return res.json();
+	return res.data;
 };
 
 export const getGeneratedImage = async (
@@ -19,15 +18,12 @@ export const getGeneratedImage = async (
 	size = '512x512',
 	n = 1,
 ) => {
-	const res = await fetch('/api/image', {
-		method: 'POST',
-		body: JSON.stringify({
-			prompt,
-			size,
-			n,
-			model: getConfig('model'),
-		}),
+	const res = await axios.post('/api/image', {
+		prompt,
+		size,
+		n,
+		model: getConfig('model'),
 	});
 
-	return res.json();
+	return res.data;
 };
