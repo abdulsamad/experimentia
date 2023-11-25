03c1b2f548f36826ab5e100fc6474905089d739e
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-11-25 19:15:40 +0530

    fix: cursor position reset after rerender

--- apps/client/src/hooks/useCustomTiptapEditor.tsx
+++ apps/client/src/hooks/useCustomTiptapEditor.tsx
@@ -44,8 +44,13 @@ const useCustomTiptapEditor = () => {
   useEffect(() => {
     if (!editor) return;
 
+    const cursorPos = editor.state.selection.$head.pos;
+
     editor?.commands?.clearContent();
     editor?.commands.insertContent(state);
+
+    // Reset cursor position after inserting content
+    editor.chain().focus().setTextSelection(cursorPos).run();
   }, [state, editor]);
 
   const handleSubmit = useCallback(async () => {
