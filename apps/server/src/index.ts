5663237aeea6d46ef2d1450afda36a11d992ee4b
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-11-26 10:47:31 +0530

    refactor: Removed unwanted cors

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -17,11 +17,7 @@ const jwtCheck = auth({
 });
 
 const corsOption = {
-  origin: [
-    'http://localhost:3000',
-    'https://experimentia.netlify.app',
-    'https://experimentia.vercel.app',
-  ],
+  origin: ['http://localhost:3000', 'https://experimentia.netlify.app'],
 };
 
 app.use(express.json());
