6072a75a5d8750a7e9b7c23bd017bec4e2ca184d
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-11-27 11:02:17 +0530

    refactor: Updated .env types for client

--- apps/client/global.d.ts
+++ apps/client/global.d.ts
@@ -1,9 +1,12 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    OPENAI_API_KEY: string;
+    AUTH0_BASE_URL: string;
+    AUTH0_CLIENT_ID: string;
+    AUTH0_CLIENT_SECRET: string;
+    AUTH0_ISSUER_BASE_URL: string;
+    AUTH0_SECRET: string;
     NEXT_PUBLIC_CONFIGCAT_API_KEY: string;
-    // AUTH0_CLIENT_ID: string;
-    // AUTH0_CLIENT_SECRET: string;
     NEXT_PUBLIC_SERVER_ENDPOINT: string;
+    OPENAI_API_KEY: string;
   }
 }
