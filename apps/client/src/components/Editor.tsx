d719e75c0aeeedef532e4186ede67984ef4efc80
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-12-04 14:40:39 +0530

    fix: Moved sonner toast to root layout

--- apps/client/src/components/Editor.tsx
+++ apps/client/src/components/Editor.tsx
@@ -1,5 +1,4 @@
 import { useAtomValue } from 'jotai';
-import { Toaster } from 'sonner';
 
 import { configAtom } from '@/store';
 import Text from '@/components/Inputs/Text';
@@ -8,12 +7,7 @@ import Voice from '@/components/Inputs/Voice';
 const Editor = () => {
   const { textInput } = useAtomValue(configAtom);
 
-  return (
-    <div className="flex flex-col">
-      {textInput ? <Text /> : <Voice />}
-      <Toaster richColors />
-    </div>
-  );
+  return <div className="flex flex-col">{textInput ? <Text /> : <Voice />}</div>;
 };
 
 export default Editor;
