48e0d4783c065ebab7650921133fb008865d9628
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-12-04 15:56:48 +0530

    refactor: Renamed home page

--- apps/client/src/components/Home.tsx
+++ apps/client/src/components/Home.tsx
@@ -4,10 +4,10 @@ import { configAtom } from '@/store';
 import Text from '@/components/Inputs/Text';
 import Voice from '@/components/Inputs/Voice';
 
-const Editor = () => {
+const Home = () => {
   const { textInput } = useAtomValue(configAtom);
 
   return <div className="flex flex-col">{textInput ? <Text /> : <Voice />}</div>;
 };
 
-export default Editor;
+export default Home;
