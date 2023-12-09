e092ef1ed3fa9458cfcc6034e51bbb3874e4abcf
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-12-09 16:54:19 +0530

    refactor: Remove voice input feature from browser that does not supports it

--- apps/client/src/components/Home.tsx
+++ apps/client/src/components/Home.tsx
@@ -3,11 +3,16 @@ import { useAtomValue } from 'jotai';
 import { configAtom } from '@/store';
 import Text from '@/components/Inputs/Text';
 import Voice from '@/components/Inputs/Voice';
+import { IS_SPEECH_RECOGNITION_SUPPORTED } from '@/utils';
 
 const Home = () => {
   const { textInput } = useAtomValue(configAtom);
 
-  return <div className="flex flex-col">{textInput ? <Text /> : <Voice />}</div>;
+  return (
+    <div className="flex flex-col">
+      {!textInput && IS_SPEECH_RECOGNITION_SUPPORTED() ? <Voice /> : <Text />}
+    </div>
+  );
 };
 
 export default Home;
