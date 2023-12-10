3f78c0181f9f08e5eb8b14348e4c535eccb41679
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-12-10 19:01:47 +0530

    fix: Image layout shift while image is loading

--- apps/client/src/components/Chats/Image.tsx
+++ apps/client/src/components/Chats/Image.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useAtomValue } from 'jotai';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { toast } from 'sonner';
 import { Download, Copy } from 'lucide-react';
@@ -6,6 +7,7 @@ import ImageGallery from 'react-image-gallery';
 
 import 'react-image-gallery/styles/css/image-gallery.css';
 
+import { configAtom } from '@/store';
 import { Button } from '@/components/ui/button';
 import {
   Accordion,
@@ -20,10 +22,15 @@ interface IImage {
 }
 
 const Image = ({ src, alt }: IImage) => {
+  const { imageSize } = useAtomValue(configAtom);
+
+  const width = parseInt(imageSize.split('x')[0]);
+  const height = parseInt(imageSize.split('x')[1]);
+
   return (
     <div className="group max-w-[400px]">
       <figure>
-        <div className="relative inline-block">
+        <div style={{ width, height }} className="relative inline-block">
           <ImageGallery
             items={[
               { original: src as string, originalAlt: alt, originalClass: 'image-gallery-image' },
