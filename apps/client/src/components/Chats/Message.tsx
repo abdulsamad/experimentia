3f7e450bebde48735e98350162180b20b9d412e8
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-12-10 17:17:19 +0530

    fix: Not parsing html properly

--- apps/client/src/components/Chats/Message.tsx
+++ apps/client/src/components/Chats/Message.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { toast } from 'sonner';
 import { Copy } from 'lucide-react';
+import parse from 'html-react-parser';
 
 import { Button } from '@/components/ui/button';
 
@@ -15,9 +16,9 @@ const Message = ({ isUser, messageClassNames, message }: IMessage) => {
   return (
     <div className="relative">
       <span
-        className={`message relative inline-block max-w-[400px] py-1.5 px-3 rounded-xl before:content-[''] before:block before:h-0 before:w-0 before:border-y-8 before:border-y-transparent before:border-l-[14px] before:border-l-primary before:absolute before:top-1/2 before:-translate-y-1/2 ${messageClassNames}`}
-        dangerouslySetInnerHTML={{ __html: message as string }}
-      />
+        className={`message relative inline-block max-w-[400px] py-1.5 px-3 rounded-xl before:content-[''] before:block before:h-0 before:w-0 before:border-y-8 before:border-y-transparent before:border-l-[14px] before:border-l-primary before:absolute before:top-1/2 before:-translate-y-1/2 ${messageClassNames}`}>
+        {parse(message as string)}
+      </span>
       {!isUser && (
         <CopyToClipboard text={message as string} onCopy={() => toast.success('Copied!')}>
           <Button
