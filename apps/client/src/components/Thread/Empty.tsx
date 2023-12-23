725854bfcaa00dbd63d567a91f6df5d3b29dd5e1
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-12-23 19:28:17 +0530

    chore: Added hint for empty thread

--- apps/client/src/components/Thread/Empty.tsx
+++ apps/client/src/components/Thread/Empty.tsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useAtomValue } from 'jotai';
+
+import { variations } from 'utils';
+
+import { configAtom } from '@/store';
 
 interface IEmpty {
   nickname: string | null | undefined;
@@ -6,10 +11,14 @@ interface IEmpty {
 }
 
 const Empty = ({ nickname, textInput }: IEmpty) => {
+  const { variation } = useAtomValue(configAtom);
+
+  const hint = useMemo(() => variations.find(({ code }) => code === variation)?.hint, [variation]);
+
   return (
     <div className="flex justify-center items-center min-h-[250px] bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
+      <div className="text-center">
+        <div className="max-w-md mx-auto mt-16">
           <h1 className="text-5xl font-bold capitalize break-all">
             Hello {nickname || 'there'},{' '}
             <span className="animate-wave origin-[70%_70%] inline-block">👋</span>
@@ -20,6 +29,7 @@ const Empty = ({ nickname, textInput }: IEmpty) => {
               : `Tap the mic button in the bottom right corner and start speaking. You can also change settings from the hamburger menu in the top left corner.`}
           </h2>
         </div>
+        {hint && <div className="my-6">{hint}</div>}
       </div>
     </div>
   );
