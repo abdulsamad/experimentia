7e25b7a3c7872a0a0c579a70a08d90ebbac0af22
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-12-29 00:49:01 +0530

    feat: Added copy button for user messages

--- apps/client/src/components/Thread/Message/Text.tsx
+++ apps/client/src/components/Thread/Message/Text.tsx
@@ -19,16 +19,16 @@ const Text = ({ isUser, messageClassNames, message }: IText) => {
         className={`message relative inline-block max-w-[400px] py-1.5 px-3 rounded-xl before:content-[''] before:block before:h-0 before:w-0 before:border-y-8 before:border-y-transparent before:border-l-[14px] before:border-l-primary before:absolute before:top-1/2 before:-translate-y-1/2 ${messageClassNames}`}>
         {parse(message as string)}
       </span>
-      {!isUser && (
-        <CopyToClipboard text={message as string} onCopy={() => toast.success('Copied!')}>
-          <Button
-            title="Copy"
-            size="icon"
-            className="h-6 w-6 absolute bottom-0 -right-8 -translate-y-1/2 group-hover/chat:visible invisible">
-            <Copy className="h-4 w-4" />
-          </Button>
-        </CopyToClipboard>
-      )}
+      <CopyToClipboard text={message as string} onCopy={() => toast.success('Copied!')}>
+        <Button
+          title="Copy"
+          size="icon"
+          className={`h-6 w-6 -translate-y-1/2 group-hover/chat:visible invisible absolute bottom-0 ${
+            isUser ? '-left-8' : '-right-8'
+          }`}>
+          <Copy className="h-4 w-4" />
+        </Button>
+      </CopyToClipboard>
     </div>
   );
 };
