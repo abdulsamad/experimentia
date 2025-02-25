a2df7d03384accefb173be959efbaa285ec8ecdc
Abdul Samad Ansari <hello@abdulsamad.dev>
2025-02-26 01:40:51 +0530

    refactor: updated global.d.ts

--- apps/client/global.d.ts
+++ apps/client/global.d.ts
@@ -13,28 +13,24 @@ declare namespace NodeJS {
 // react-image-gallery
 declare module 'react-image-gallery' {
   import { Component } from 'react';
-  export default class ReactImageGallery extends Component<any> {
-    props: any;
-    constructor(props: any);
-    render(): JSX.Element;
-  }
+  export default class ReactImageGallery extends Component<any> {}
 }
 
 // use-sound
 declare module 'use-sound' {
-  export const useSound = (filepath: string) => [(play = () => null)];
+  type PlayFunction = () => void;
+  export function useSound(filepath: string): [PlayFunction];
 }
 
 // react-copy-to-clipboard
 declare module 'react-copy-to-clipboard' {
   import * as React from 'react';
-  export const CopyToClipboard = ({
-    children,
-    text,
-    onCopy,
-  }: {
-    children: React.Node;
+
+  interface CopyToClipboardProps {
+    children: React.ReactNode;
     text: string;
-    onCopy: func;
-  }) => children;
+    onCopy?: (text: string, result: boolean) => void;
+  }
+
+  export class CopyToClipboard extends React.Component<CopyToClipboardProps> {}
 }
