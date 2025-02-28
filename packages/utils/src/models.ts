a112479e9d4689ae6740fe6d5a5e137564b4ed55
Abdul Samad Ansari <hello@abdulsamad.dev>
2025-03-01 01:40:12 +0530

    refactor: image size on global config

--- packages/utils/src/models.ts
+++ packages/utils/src/models.ts
@@ -274,3 +274,17 @@ export const variations = [
     ],
   },
 ] as const;
+
+export const imageSizes = (model: string) => {
+  if (model === 'dall-e-3') {
+    return {
+      default: '1024x1024',
+      options: ['1024x1024', '1024x1792', '1792x1024'],
+    };
+  }
+
+  return {
+    default: '256x256',
+    options: ['256x256', '512x512', '1024x1024'],
+  };
+};
