f11b4cd5fec2b819c891ad9fb0981abd29192473
Abdul Samad Ansari <hello@abdulsamad.dev>
2025-03-01 17:19:10 +0530

    chore: remove cors middleware

--- apps/serverless/src/index.ts
+++ apps/serverless/src/index.ts
@@ -1,14 +1,12 @@
 import { Hono } from 'hono';
 import { streamHandle } from 'hono/aws-lambda';
 import { logger } from 'hono/logger';
-import { cors } from 'hono/cors';
 
 import chat from 'controllers/chat';
 import { authMiddleware } from '@middlewares/index';
 
 export const app = new Hono();
 
-app.use(cors({ origin: [process.env.ALLOWED_ORIGINS], allowMethods: ['GET', 'POST'] }));
 app.use(logger());
 app.use(authMiddleware);
 
