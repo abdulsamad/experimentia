4cd28b7900ee1fde90028fd8473acbe7f7b325ee
Abdul Samad Ansari <hello@abdulsamad.dev>
2025-03-02 07:54:41 +0530

    fix: navbar styles

--- apps/client/app/components/Nav/index.tsx
+++ apps/client/app/components/Nav/index.tsx
@@ -4,19 +4,17 @@ import { SidebarTrigger } from '@/components/ui/sidebar';
 
 import SettingsDropdown from './SettingsDropdown';
 
-const Nav = () => {
-  return (
-    <nav className="h-[80px] w-full flex gap-2 items-center justify-center relative">
-      <SidebarTrigger className="absolute top-0 left-0 ml-5 mt-5 py-1 px-2.5" />
-      <motion.h1
-        initial={{ translateY: '-10px' }}
-        animate={{ translateY: 0 }}
-        className="text-2xl lg:text-3xl italic text-center font-bold from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent">
-        Experimentia
-      </motion.h1>
-      <SettingsDropdown />
-    </nav>
-  );
-};
+const Nav = () => (
+  <nav className="h-[50px] w-full flex gap-2 items-center justify-between relative px-3 py-1">
+    <SidebarTrigger />
+    <motion.h1
+      initial={{ translateY: '-10px' }}
+      animate={{ translateY: '0' }}
+      className="text-lg lg:text-xl italic font-bold from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent leading-6">
+      Experimentia
+    </motion.h1>
+    <SettingsDropdown />
+  </nav>
+);
 
 export default Nav;
