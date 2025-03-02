1a45ab4f1c30936608ca0e5e1e29a167d21a315e
Abdul Samad Ansari <hello@abdulsamad.dev>
2025-03-02 20:03:01 +0530

    feat: vite plugin pwa

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,10 +1,16 @@
-import { reactRouter } from '@react-router/dev/vite';
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import { reactRouter } from '@react-router/dev/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    VitePWA({ registerType: 'autoUpdate' }) as any,
+  ],
   optimizeDeps: {
     include: ['react-syntax-highlighter/dist/cjs/styles/prism'],
   },
