4e0d2a179984852fa7ef2dd2cb0c1590f8decb70
Abdul Samad Ansari <hello@abdulsamad.dev>
2025-03-05 01:01:37 +0530

    refactor: remove unwanted local file

--- apps/serverless/src/local.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from '@hono/node-server';
-
-import { app } from './index.js';
-
-const PORT = process.env.PORT || 3001;
-
-console.log(`Server is running on port ${PORT}`);
-
-serve({ fetch: app.fetch, port: PORT });
